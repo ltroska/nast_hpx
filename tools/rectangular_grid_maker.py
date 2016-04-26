906288dd1007d816f3f8dd83cb6da354010ba6a7
Lukas Troska <lukas.troska@gmail.com>
2016-04-26 22:06:05 +0200

    added script to create rectangular grids, added bigger driven cavity example

---
+++ tools/rectangular_grid_maker.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import sys
+
+import csv
+
+if len(sys.argv) != 4:
+	print("Usage:", sys.argv[0] , "n m <output grid>", file=sys.stderr)
+	sys.exit(0)
+
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+outfile_path = sys.argv[3]
+
+with open(outfile_path, 'wb') as csvfile:
+	gridwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+
+	for j in range(m):
+		row = []
+		if j == 0 or j == m - 1:
+			row = [1]*n
+
+		else:
+			row.append(1)
+			row += [0]*(n-2)
+			row.append(1)
+
+		gridwriter.writerow(row)
+
+

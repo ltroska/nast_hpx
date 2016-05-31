30af2b3541f4cd7c0c635025a2f2a5fc6298d2c8
Lukas Troska <troska@ins.uni-bonn.de>
2016-05-31 17:03:44 +0200

    Clean up main

--- src/nast_hpx.cpp
+++ src/nast_hpx.cpp
@@ -4,20 +4,12 @@
 #include "io/config.hpp"
 #include "stepper/stepper.hpp"
 
-#include <hpx/lcos/when_each.hpp>
-
 int hpx_main(boost::program_options::variables_map& vm)
 {
    nast_hpx::stepper::stepper step;
 
-  /*  std::vector<hpx::future<int> > a;
-    a.push_back(hpx::make_ready_future(1));
-    a.push_back(hpx::make_ready_future(2));
-
-    auto f1 = [](hpx::future<int> f) {std::cout << "got " << f.get() << " on first " << std::endl;};
-    auto f2 = [](int idx, hpx::future<int> f) {std::cout << "got " << f.get() << " with index " << idx << "on second" << std::endl;};
-
-    hpx::when_each_n(f2, a.begin(), 1);*/
+    std::cout << "Threads on locality " << hpx::get_locality_id()
+        << " = " << hpx::threads::hardware_concurrency() << std::endl;
 
     const auto cfg_path = vm["cfg"].as<std::string>();
 
@@ -38,7 +30,6 @@ int main(int argc, char* argv[])
 
     std::vector<std::string> cfg;
     cfg.push_back("hpx.run_hpx_main!=1");
-
 
     return hpx::init(desc_commandline, argc, argv, cfg);
 }
